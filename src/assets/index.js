5ba7d4bf7f6b2087cad84385b7095cf47ab221da
AlAgilly <alagilly@my.yorku.ca>
2023-01-12 15:41:44 -0500

    ;.;

---
+++ src/assets/index.js
@@ -0,0 +1,81 @@
+import airbnb from "./airbnb.png";
+import about from "./about.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./mainlogo.svg";
+import logoline from "./logoline.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import dota2 from "./games/dota2.png";
+import dota2a from "./games/dota2-active.png";
+import dota2g from "./games/dota2-grey.png";
+import logooutline from "./logo-outline.svg";
+import post1 from "./post1.png";
+import post2 from "./post2.png";
+import post3 from "./post3.png";
+import post4 from "./post4.png";
+import post5 from "./post5.png";
+import post6 from "./post6.png";
+import post7 from "./post7.png";
+import post8 from "./post8.png";
+
+
+export {
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  quotes,
+  robot,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+  dota2,
+  dota2a,
+  dota2g,
+  logooutline,
+  about,
+  post1,
+  post2,
+  post3,
+  post4,
+  post5,
+  post6,
+  post7,
+  post8,
+};
