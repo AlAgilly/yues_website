616b0e7b3166a3f58f83b8beb79759a2f8829f51
Anujpreet19 <anujpreet190602@gmail.com>
2023-01-26 11:01:48 -0500

    working on shadows

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,7 +14,7 @@ import {
 function Contact() {
   return (
     <>
-      <div className="w-full overflow-hidden bg-white z-20">
+      <div className="w-full overflow-hidden bg-white z-20`">
         <div className={`idk2 ${styles.flexStart} `}>
           <div className={`p-0 max-w-[1340px] w-full py-10`}>
             <div
@@ -32,49 +32,44 @@ function Contact() {
         </div>
         {/* our club snip */}
 
-        <div className="w-full overflow-hidden bg-white z-20 ">
-          <div className={`grid grid-row-2 grid-flow-row `}>
-            <div className={`left pt-40 pl-96`}>
-              <img src={borderlines2} alt="" />
-            </div>
-            <div className={`right pt-[30px] pr-[620px]`}>
-              <img src={rec50} alt="" className={``} />
-            </div>
+        <div className={`grid grid-row-2 grid-flow-row `}>
+          <div className={`left pt-40 pl-96`}>
+            <img src={borderlines2} alt="" />
+          </div>
+          <div className={`right pt-[30px] pr-[620px]`}>
+            <img src={rec50} alt="" className={``} />
+          </div>
 
-            <div className={`${styles.flexStart}`}>
-              <div className={`${styles.boxWidth}`}>
-                <div
-                  className={`${styles.titleH3} leading-10 text-black -ml-[20px]`}
-                >
-                  Our club
-                  <div className={`${styles.titleLineBottom} pt-2`}></div>
-                </div>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex
-                  et ratione doloribus omnis earum. Facere qui, ab ipsam tempora
-                  veniam, culpa ea ipsum accusantium eligendi repellat saepe
-                  soluta corporis ad dolor nulla molestias? Aperiam obcaecati
-                  exercitationem ullam quae modi perferendis soluta alias
-                  nesciunt! Unde hic voluptate facere dolorem placeat eaque{" "}
-                  <br />
-                  <br />
-                  accusantium consequatur provident quis. Magni veritatis ab
-                  nisi magnam cumque, corporis repellendus quod sint quasi
-                  officiis, quae officia repellat suscipit cum et dolorum
-                  reprehenderit dignissimos distinctio voluptatum fugit laborum
-                  sunt autem. Excepturi, cumque? Illum sequi dignissimos
-                  molestias unde consequatur aliquam neque nemo iusto, at
-                  eligendi, nam quas nisi dolorum placeat.
-                </p>
+          <div className={`${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+              <div
+                className={`${styles.titleH3} leading-10 text-black -ml-[20px]`}
+              >
+                Our club
+                <div className={`${styles.titleLineBottom} pt-2`}></div>
               </div>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex et
+                ratione doloribus omnis earum. Facere qui, ab ipsam tempora
+                veniam, culpa ea ipsum accusantium eligendi repellat saepe
+                soluta corporis ad dolor nulla molestias? Aperiam obcaecati
+                exercitationem ullam quae modi perferendis soluta alias
+                nesciunt! Unde hic voluptate facere dolorem placeat eaque <br />
+                <br />
+                accusantium consequatur provident quis. Magni veritatis ab nisi
+                magnam cumque, corporis repellendus quod sint quasi officiis,
+                quae officia repellat suscipit cum et dolorum reprehenderit
+                dignissimos distinctio voluptatum fugit laborum sunt autem.
+                Excepturi, cumque? Illum sequi dignissimos molestias unde
+                consequatur aliquam neque nemo iusto, at eligendi, nam quas nisi
+                dolorum placeat.
+              </p>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* our staff 2023 snip */}
+        {/* our staff 2023 snip */}
 
-      <div className="w-full overflow-hidden bg-white z-20 ">
         <div className={`${styles.flexStart}`}>
           <div className={`${styles.boxWidth}`}>
             <div className={` `}>
@@ -90,8 +85,7 @@ function Contact() {
                 </a>
               </p>
               {/* panel format */}
-              <div className={`${styles.titleH4}  `}>
-                <br />
+              <div className={`${styles.titleH4}  pt-10`}>
                 co-presidents
                 <div className={`grid grid-cols-3 gap-36 pb-10`}>
                   {coprezpanel.map((stf, index) => (
@@ -104,7 +98,6 @@ function Contact() {
               </div>
               {/* panel format */}{" "}
               <div className={`${styles.titleH4}  `}>
-                <br />
                 Executives
                 <div className={`grid grid-cols-3 gap-36 pb-10`}>
                   {execpanel.map((stf, index) => (
@@ -116,7 +109,6 @@ function Contact() {
                 </div>
               </div>{" "}
               <div className={`${styles.titleH4}  `}>
-                <br />
                 Operations Department
                 <div className={`grid grid-cols-3 gap-36 pb-10`}>
                   {oppanel.map((stf, index) => (
@@ -128,7 +120,6 @@ function Contact() {
                 </div>
               </div>{" "}
               <div className={`${styles.titleH4}  `}>
-                <br />
                 Parternerships Department
                 <div className={`grid grid-cols-3 gap-36 pb-10`}>
                   {partpanel.map((stf, index) => (
@@ -140,7 +131,6 @@ function Contact() {
                 </div>
               </div>{" "}
               <div className={`${styles.titleH4}  `}>
-                <br />
                 Marketing department
                 <div className={`grid grid-cols-3 gap-36 pb-10`}>
                   {marketpanel.map((stf, index) => (
@@ -164,7 +154,6 @@ function Contact() {
                 </div>
               </div>{" "}
               <div className={`${styles.titleH4}  `}>
-                <br />
                 Human resources department
                 <div className={`grid grid-cols-3 gap-36 pb-10`}>
                   {hrpanel.map((stf, index) => (
