e9f08dae64e8e9a81aabf9e4ac10f6aa53205ded
Lynn Al Agilly <lynn.alagilly@yahoo.ca>
2023-04-21 11:18:30 -0400

    client

---
+++ client/src/components/HoverImage.jsx
@@ -0,0 +1,82 @@
+// import React from "react";
+
+// export type HoverImageProps = {
+//   src: string;
+//   hoverSrc: string;
+//   disabled?: boolean;
+//   className?: string;
+//   style?: any;
+//   onClick?: React.MouseEventHandler;
+// };
+// const HoverImage: React.FC<HoverImageProps> = ({
+//   src,
+//   hoverSrc,
+//   style,
+//   disabled,
+//   onClick,
+//   className,
+// }) => {
+//   const [imageSrc, setImageSrc] = React.useState<string>(src);
+
+//   const mouseOver = React.useCallback(() => {
+//     setImageSrc(hoverSrc);
+//   }, []);
+
+//   const mouseOut = React.useCallback(() => {
+//     setImageSrc(src);
+//   }, []);
+
+//   const handleClick = (e: React.MouseEvent) => {
+//     if (!onClick) return;
+//     if (!disabled) {
+//       onClick(e);
+//     }
+//   };
+
+//   return (
+//     <img
+//       src={imageSrc}
+//       style={style}
+//       onMouseOver={mouseOver}
+//       onMouseOut={mouseOut}
+//       onClick={handleClick}
+//       className={className}
+//     />
+//   );
+// };
+
+// export default HoverImage;
+
+import React from "react";
+
+const HoverImage = ({ src, hoverSrc, style, disabled, onClick, className }) => {
+  const [imageSrc, setImageSrc] = React.useState(src);
+
+  const mouseOver = React.useCallback(() => {
+    setImageSrc(hoverSrc);
+  }, []);
+
+  const mouseOut = React.useCallback(() => {
+    setImageSrc(src);
+  }, []);
+
+  const handleClick = (e) => {
+    if (!onClick) return;
+    if (!disabled) {
+      onClick(e);
+    }
+  };
+
+  return (
+    <img
+      src={imageSrc}
+      style={style}
+      onMouseOver={mouseOver}
+      onMouseOut={mouseOut}
+      onClick={handleClick}
+      className={className}
+    />
+  );
+};
+
+export default HoverImage;
