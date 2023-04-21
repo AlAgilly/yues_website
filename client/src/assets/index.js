2775b0a29de8bb7b92a337ed754e7b4f2ac837e0
Lynn Al Agilly <lynn.alagilly@yahoo.ca>
2023-04-21 11:18:15 -0400

    client

---
+++ client/src/assets/index.js
@@ -0,0 +1,70 @@
+// PNGs - Images
+import aboutsnip from './aboutsnip.png'
+import bordercenter from './bordercenter.png'
+import borderline from './borderline.png'
+import eventsnip from './eventsnip.png'
+import headerimg from './headerimg.png'
+import herobg from './herobg.png'
+import swirl from './swirl.png'
+import team1 from './team1.png'
+import team2 from './team2.png'
+import whitelion from './whitelion.png'
+import yulion from './yulion.png'
+import dota2 from './games/dota2.png'
+import dota2a from './games/dota2-active.png'
+import dota2g from './games/dota2-grey.png'
+import grey from './grey.png'
+import bgg from './bgg.png'
+
+// SVGs - Vectors
+import arrowup from "./arrow-up.svg"
+import close from "./close.svg"
+import logo from "./logo.svg"
+import greyoutline from "./greyoutline.svg"
+import logoline from "./logoline.svg"
+import outline from "./logo-outline.svg"
+import mainlogo from "./mainlogo.svg"
+import menu from "./menu.svg"
+import out from "./outt.svg"
+import yuesports from "./yuesports.svg"
+import bordercenters from './bordercenters.svg'
+import borderlines from './borderlines.svg'
+import borderlines2 from './borderlines2.svg'
+import droparrow from './droparrow.svg'
+import staffbox from './staffbox.svg'
+import staffbox2 from './staffbox.png'
+
+export {
+  aboutsnip,
+  bordercenter,
+  borderline,
+  bordercenters,
+  borderlines,
+  borderlines2,
+  eventsnip,
+  headerimg,
+  herobg,
+  swirl,
+  team1,
+  team2,
+  whitelion,
+  yulion,
+  greyoutline,
+  dota2,
+  dota2a,
+  arrowup,
+  close,
+  logo,
+  logoline,
+  outline,
+  mainlogo,
+  menu,
+  dota2g,
+  out,
+  grey,
+  yuesports,
+  droparrow,
+  staffbox,
+  staffbox2,
+  bgg,
+};
