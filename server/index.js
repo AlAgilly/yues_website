0cda8ae85364ae0d3416d36d755d2f6242bd769c
Lynn Al Agilly <lynn.alagilly@yahoo.ca>
2023-04-22 12:27:03 -0400

    localhost api works

---
+++ server/index.js
@@ -0,0 +1,23 @@
+
+import express from "express";
+import http from "http";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import { port, allowedDomains } from "./config/index.js";
+import posts from "./data/index.js"
+
+const app = express();
+
+app.use(cors({origin: allowedDomains}))
+app.use(helmet())
+app.use(compression())
+
+app.get('/api/posts', (req, res) => {
+    return res.json(posts)
+})
+
+const server = http.createServer(app);
+server.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`)
+})
