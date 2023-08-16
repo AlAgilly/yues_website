641715fdb92c4a921b8dfe51957d24145cb3d166
Isaac <Scaiaaguinaga@gmail.com>
2023-08-15 19:16:21 -0700

    Temporarily finished front end for contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -17,9 +17,9 @@ function Contact() {
     <>
         {/* Title */}
         <Header title="Contact" />
-        <div className={`max-w-[1340px] m-auto ${styles.paddingX} ${styles.paddingY}`}>
-          <div>
-            <h6>
+        <div className={`max-w-[1340px] m-auto px-12 xs:px-24 py-8`}>
+          <div className={`w-full mm:w-2/3 pb-[40px]`}>
+            <h6 className={`font-bevietnamRegular font-bold py-2 text-lg`}>
               For general inquiries please contact:
             </h6>
             <p>
@@ -29,23 +29,27 @@ function Contact() {
               esports@my.yorku.ca
             </p>
           </div>
-          <div>
-            <h6>
+          <div className="w-full mm:w-2/3">
+            <h6 className="font-bevietnam font-bold pb-2">
               For specialized inquiries you can find us on Discord
             </h6>
-            <p>Alexander Saiko | Co-President</p>
-            <p>Lovesaik#1622</p>
-            <p>Alexander Saiko | Co-President</p>
-            <p>Lovesaik#1622</p>
-            <p>Alexander Saiko | Co-President</p>
-            <p>Lovesaik#1622</p>
-            <p>Alexander Saiko | Co-President</p>
-            <p>Lovesaik#1622</p>
-            <p>Alexander Saiko | Co-President</p>
-            <p>Lovesaik#1622</p>
-            <p>Alexander Saiko | Co-President</p>
-            <p>Lovesaik#1622</p>
-            <p>Alexander Saiko | Co-President</p>
+            <p className="text-red">Alexander Saiko | Co-President</p>
+            <p className="pb-2">Lovesaik#1622</p>
+            <p className="text-red">Sarah Miller | Co-President</p>
+            <p className="pb-2">ParallelNymph#2742</p>
+            <p className="text-red">Ismaeel Jafar Abdus-Samad | Vice-President of Operations</p>
+            <p className="pb-2">TheSmiley03#2025</p>
+            <p className="text-red">Alexander Saiko | Co-President</p>
+            <p className="pb-2">Lovesaik#1622</p>
+            <p className="text-red">Alexander Saiko | Co-President</p>
+            <p className="pb-2">Lovesaik#1622</p>
+            <p className="text-red">Alexander Saiko | Co-President</p>
+            <p className="pb-2">Lovesaik#1622</p>
+          </div>
+          <div className="w-full mm:w-2/3 py-8">
+            <h6 className="font-bevietnam font-bold">Club Room</h6>
+            <p>York University’s Second Student Centre<br/>4700 Keele St #335, North York, ON M3J 1P3</p>
+            <p>Room 316</p>
           </div>
         </div>
     </>
