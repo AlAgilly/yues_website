60f5e5be456a688ad364c50cb430566d13854df4
Isaac <Scaiaaguinaga@gmail.com>
2023-08-15 17:57:20 -0700

    Began work on contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -17,6 +17,37 @@ function Contact() {
     <>
         {/* Title */}
         <Header title="Contact" />
+        <div className={`max-w-[1340px] m-auto ${styles.paddingX} ${styles.paddingY}`}>
+          <div>
+            <h6>
+              For general inquiries please contact:
+            </h6>
+            <p>
+              yorkesports@gmail.com
+            </p>
+            <p>
+              esports@my.yorku.ca
+            </p>
+          </div>
+          <div>
+            <h6>
+              For specialized inquiries you can find us on Discord
+            </h6>
+            <p>Alexander Saiko | Co-President</p>
+            <p>Lovesaik#1622</p>
+            <p>Alexander Saiko | Co-President</p>
+            <p>Lovesaik#1622</p>
+            <p>Alexander Saiko | Co-President</p>
+            <p>Lovesaik#1622</p>
+            <p>Alexander Saiko | Co-President</p>
+            <p>Lovesaik#1622</p>
+            <p>Alexander Saiko | Co-President</p>
+            <p>Lovesaik#1622</p>
+            <p>Alexander Saiko | Co-President</p>
+            <p>Lovesaik#1622</p>
+            <p>Alexander Saiko | Co-President</p>
+          </div>
+        </div>
     </>
   );
 }
